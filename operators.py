9457298ca1280210267ed7c74d229fce47919265
Mohamed Yunus <yunus.bj.my@gmail.com>
2025-07-22 18:56:44 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,32 @@
+# Arithmetic	Basic math operations	+, -, *, /, %, //, **
+# Two Value get from an input and perform all Arithmetic operators
+a = int(input("Enter NUmber1: ")) 
+b = int(input("Enter NUmber2: "))
+print (f"{a} + {b} = {a+b}") #➕ + (Addition)
+print (f"{a} - {b} = {a-b}")  #➖ - (Subtraction)
+print (f"{a} * {b} = {a*b}") #✖️ * (Multiplication)
+print (f"{a} / {b} = {a/b}") #➗ / (Division)
+print (f"{a} % {b} = {a%b}") # % (Modulus)
+print (f"{a} // {b} = {a//b}") # // (Floor Division) - Divides and returns only the integer part (floor value).
+print (f"{a} ** {b} = {a**b}") # ** (Exponentiation) - Raises the left operand to the power of the right operand.
+
+# Comparison	Compare two values	==, !=, >, <, >=, <=
+a = int(input("Enter NUmber1: ")) 
+b = int(input("Enter NUmber2: "))
+print (f"answer is {a==b}") 
+print (f"answer is {a<=b}")  
+print (f"answer is {a>=b}") 
+print (f"answer is {a!=b}") 
+print (f"answer is {a<b}")  
+print (f"answer is {a>b}")
+
+
+
+# Logical	Combine conditional statements	
+
+
+
+# Assignment	Assign values to variables	=, +=, -=, *=
+# Bitwise	Operate on bits	`&,
+# Membership	Check for membership in a sequence	in, not in
+# Identity	Compare object identities	is, is not
