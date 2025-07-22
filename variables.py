c40f9e50c3766f118e469ed82055c760514dbd05
Mohamed Yunus <yunus.bj.my@gmail.com>
2025-07-22 18:32:57 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,19 +1,56 @@
 # Create a variable to store your name and print it?
 
+name = ("yunus") or name = "yunus" or name = 'yunus'
+print (name)
+
 # Assign your age to a variable and print it?
+age = 27
+print (age)
 
 # Store your height in a variable and print it?
+height = 5.11
+print (height)
 
 # Assign True to a variable indicating if you're a student and print it?
+student = True
+print (student)
 
 # Create two variables a = 10 and b = 20, then print their sum?
+a = 10
+b = 20
+print (a+b)
+c = a+b
+print (c)
 
-# Change the value of a variable x = 5 to x = 10 and print the new value?
+# Change the value of a variable x = 5 to y = 10 and print the new value?
+x = 5
+y = 10
+z = x
+x = y
+y = z
+print (f" x = {x}, y = {y}")
 
 # Create variables for your name, age, and city, then print a sentence using all three?
+name = "yunus" 
+age = 27
+city = "coimbatore"
+print(f"I Am {name}, {age} years old From {city}")
 
 # Swap values between two variables and print them?
+x = 5
+y = 10
+z = x
+x = y
+y = z
+print (f" x = {x}, y = {y}")
+
 
 # Assign a string to a variable and print the length of the string?
+x = "yunus"
+print (len(x))
 
 # Create a variable with a decimal value (float) and print its type?
+x = 5.10
+type(x)
+
+
